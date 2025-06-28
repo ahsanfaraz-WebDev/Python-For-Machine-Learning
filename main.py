252083a4009fdb031af1bb8527e538febc4cc8e4
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2025-06-29 02:52:53 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+is_Student=True
+is_Employee=False
+name="Ahsan"
+if is_Student:
+    print(f"{name} is a Student")
+else:
+    print(f"{name} is not a student")
+if is_Employee:
+    print(f"{name} is a Employee")
+else:
+    print(f"{name} is not a Employeee")
